807a8acdaa1bf07a4b1b2522cbded8c9644d59c1
Williams, Richard <rowwor@bellsouth.net>
2019-09-12 19:49:41 -0500

    Updates

---
+++ precache-manifest.9c89ca42b40311f3e0bef19e544b5eec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be4866864f9c91177c1ab76ac1a4c0b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "cb996f23d5191d44d37a",
+    "url": "/Clicky-Game/static/css/main.5d589b88.chunk.css"
+  },
+  {
+    "revision": "4169f8714165230877ef",
+    "url": "/Clicky-Game/static/js/2.aa40b70b.chunk.js"
+  },
+  {
+    "revision": "cb996f23d5191d44d37a",
+    "url": "/Clicky-Game/static/js/main.02b28b1a.chunk.js"
+  },
+  {
+    "revision": "ea893b6d0fdfd50bce37",
+    "url": "/Clicky-Game/static/js/runtime~main.7cfc92f9.js"
+  }
+]);
